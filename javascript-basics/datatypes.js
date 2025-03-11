d0ac16c6efcd604d381073442dc7ecca95709839
k-ankit06 <kumar.ak22ji@gmail.com>
2025-03-11 11:46:23 +0530

    datatypes

---
+++ javascript-basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using node js, not browser
+
+console.log (3+3)
+
+console.log("Ankit Kumar")
+
+
+let name ="hitesh"
+let age = 25
+let isLoggedIn = false
+
+//  number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => null standalone value
+// indefined => 
+    // symbol => unique value 
+
+
+
+
+
+// object 
+
+console.log(typeof"Ankit kumar");
---
+++ javascript-basics/var.js
@@ -0,0 +1,18 @@
+const accountId = 14334  //Pemanent Variable
+let accountEmail = "kumar.8382ankit@gmail.com" //print both new and old data
+var accountPassword = "434243" // always print new data 
+accountCity = "Dera Bassi"   // always print new data (var)
+  let accountState;
+
+
+
+
+/*
+Prefered not use var
+beacuse of issue in block scope and functional scope
+
+*/
+accountEmail = "abhjss12,ku@gmail.com"
+accountPassword = "hjgftyfv"
+accountCity ="Chandigarh"
+console.table({accountId, accountEmail, accountPassword, accountCity, accountState});
