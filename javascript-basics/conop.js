bf028a4b3eae4f3781f74bef26adddcef65614f8
k-ankit06 <kumar.ak22ji@gmail.com>
2025-03-11 12:00:58 +0530

    Conversion

---
+++ javascript-basics/conop.js
@@ -0,0 +1,23 @@
+// let score = 23
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// true =>1 ; false =>0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
